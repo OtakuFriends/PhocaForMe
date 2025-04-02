204cb169ff7080dbe1d57cb57064f020d98a46ec
유혜승 <h103906@naver.com>
2025-04-02 16:10:39 +0900

    add: pwa 및 서비스워커 세팅 추가

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,10 @@
+import withPWA from "next-pwa";
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const nextConfig: NextConfig = withPWA({
+  dest: "public", // 서비스 워커와 관련 파일들이 저장될 폴더
+  register: true, // 서비스 워커를 자동으로 등록
+  skipWaiting: true, // 새로운 서비스 워커를 즉시 활성화
+});
 
 export default nextConfig;
---
+++ frontend/public/service-worker.js
@@ -0,0 +1,12 @@
+self.addEventListener("install", (event) => {
+  console.log("Service Worker 설치됨!");
+  event.waitUntil(self.skipWaiting()); // 기존 SW 대체
+});
+
+self.addEventListener("activate", (event) => {
+  console.log("Service Worker 활성화됨!");
+});
+
+self.addEventListener("fetch", (event) => {
+  console.log("요청 캐싱:", event.request.url);
+});
