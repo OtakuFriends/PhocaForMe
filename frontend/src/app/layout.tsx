767adb1d51c8bc48b66903e5d6ba9c5286b730d0
유혜승 <h103906@naver.com>
2025-04-02 21:24:13 +0900

    fix: 파비콘 절대경로로 수정

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -4,7 +4,8 @@ export const metadata = {
   title: "포카포미",
   description: "포토카드 교환 전용 플랫폼",
   icons: {
-    icon: "public/images/icons/favicon.ico",
+    // 절대경로 들어가야 함 (나중에 깃헙 이미지 주소로 수정하기)
+    icon: "https://file.notion.so/f/f/32b54871-9d93-4376-bc12-b11af2bce837/2190b6ff-e31a-4dfd-b8cf-cbe3768d11e8/favicon.ico?table=block&id=1c9c9b3d-a83e-80a9-9d9d-f9189af9d4dd&spaceId=32b54871-9d93-4376-bc12-b11af2bce837&expirationTimestamp=1743616800000&signature=8WcEWKvx5VS9i9QSm7ZftueLJDXpDn4AyPZeuRPuPgc&downloadName=favicon.ico",
   },
 };
 
